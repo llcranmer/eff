43b0a937cb830c0e4175b75f64d79683d90f72dd
llc <touldeguiacranmer@gmail.com>
2020-02-16 13:06:01 -0600

    :sparkles: cli

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/llcranmer/eff/tcp"
 )
 
 func main() {
-	fmt.Println("hello from the main file")
-	tcp.PortScan(1024, "scanme.nmap.org")
-	fmt.Println("done with  ports scan")
+	// tcp.PortScan(1024, "127.0.0.1")
+	// fmt.Println("done with  ports scan")
 	ports := []int{80, 20, 3001}
-	tcp.SelPortScan(ports, "scanme.nmap.org")
+	tcp.SelPortScan(ports, "127.0.0.1")
 	fmt.Println("done with sel port scan")
+
+	wordPtr := flag.String("word", "foo", "a string")
+
+	numbPtr := flag.Int("numb", 42, "an int")
+	boolPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+
+	flag.Parse()
+
+	fmt.Println("word:", *wordPtr)
+	fmt.Println("numb:", *numbPtr)
+	fmt.Println("fork:", *boolPtr)
+	fmt.Println("svar:", svar)
+	fmt.Println("tail:", flag.Args())
+
+	if *wordPtr == "portScan" {
+		fmt.Println("Scanning ports..")
+		tcp.PortScan(*numbPtr, "127.0.0.1")
+		fmt.Println("done.")
+	}
 }
